542020bc5556b548437a0a6f36d3043a9d2ae56a
Dave Lewis <dil20@sussex.ac.uk>
2017-05-28 19:34:31 +0100

    Code For A Boid seeking a single target implemented

---
+++ Flocking/Assets/Scripts/Boid.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+    public float maxSpeed;
+    public float maxForce;
+    private Rigidbody rigidbody;
+
+
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+        this.rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Seek(target.position);
+        rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxSpeed);
+	}
+
+   
+    public void Flee(Vector3 ThreatPosition)
+    {
+
+    }
+
+    public void Seek(Vector3 target)
+    {
+        Vector3 desired = (target - transform.position);
+        desired.Normalize();
+        desired *= maxSpeed;
+
+        Vector3 steer = Vector3.ClampMagnitude ((desired - rigidbody.velocity),maxForce);
+
+        rigidbody.AddForce(steer);
+
+    }
+
+
+    public void Wander()
+    {
+
+    }
+
+    public void Flock()
+    {
+
+    }
+
+}
---
+++ Flocking/Assets/Scripts/FlockManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//take 3d models, replicate them multiple timmes
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//loop through models call enscary method depending on models current state.
+        //each model has its own update method?
+
+        //parent class loops through models?
+
+        //or each model has its own class
+        //in which case this class sets up the other models
+
+        //rename to FLockManager?
+
+
+        //write indivudal methods first
+        //then worry about how best to manage them
+
+	}
+
+
+   
+
+
+}

09fff8158ab7f2f6f911c79655880b7820ebcce6
Dave Lewis <dil20@sussex.ac.uk>
2017-05-29 02:53:52 +0100

    paths stil broken

--- Flocking/Assets/Scripts/Boid.cs
+++ Flocking/Assets/Scripts/Boid.cs
@@ -9,7 +9,12 @@ public class Boid : MonoBehaviour {
     public float arrivalRadius;
     private Rigidbody rigidbody;
     
+    //target for seeking or fleeing
     public Transform target;
+
+    //for path following
+    public float pathRadius;
+    public List<Transform> pathCheckPoints;
 
 	// Use this for initialization
 	void Start () {
@@ -17,10 +22,9 @@ public class Boid : MonoBehaviour {
        
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
-        Seek(target.position);
-        Debug.Log("speed:" + rigidbody.velocity.magnitude);
+        FollowPath();  
         rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxSpeed);
 	}
 
@@ -59,23 +63,98 @@ public class Boid : MonoBehaviour {
 
 
     }
-
-    //maps a value from one range to another
-    private float Map(float value,float originalMin, float originalMax, float newMin, float newMax)
-    {
-        return (newMin + (value - originalMin) * (newMax - newMin) / (originalMax - originalMin));
-    }
+     
 
     public void Wander()
     {
+        Vector3 predictedPoint =PredictedLocation(100);
+        Vector3 randomDirection= predictedPoint+(Random.onUnitSphere.normalized* 30);
+        Debug.DrawLine(transform.position, predictedPoint, Color.red);
+        Debug.DrawLine(predictedPoint, randomDirection, Color.blue);
+        Seek(randomDirection);
 
     }
 
+    public void FollowPath()
+    {
+        Vector3 predictedPoint = PredictedLocation(10);
+        Vector3 pathTarget = new Vector3(0,0,0);
+        float distanceFromNormal = 0;
+        float closestNormal = 10000;
+        for (int i = 0;i< pathCheckPoints.Count-1; i++)
+            {
+                Vector3 pathStart = pathCheckPoints[i].position;
+                Vector3 pathEnd = pathCheckPoints[i + 1].position;
+                
+                Vector3 normal = PointOfNormal(transform.position, pathStart, pathEnd);
+           
+            //check normal is on line
+            if (DistanceLineSegmentPoint(pathStart, pathEnd, normal) >0.0000000001) {
+                Debug.Log(DistanceLineSegmentPoint(normal, pathStart, pathEnd));
+               normal = pathEnd;
+            }
+            distanceFromNormal = Vector3.Distance(predictedPoint, normal);
+            Debug.DrawLine(normal, transform.position, Color.red);
+            if (distanceFromNormal < closestNormal)
+            {
+                closestNormal = distanceFromNormal;
+                pathTarget = normal;
 
+            }
+            Debug.DrawLine(pathEnd, pathStart, Color.blue);
+           
+
+
+
+        }
+        if (closestNormal > pathRadius)
+        {
+            Seek(pathTarget);
+        }
+        Debug.DrawLine(transform.position, pathTarget, Color.green);
+
+    }
+
+    public Vector3 PointOfNormal(Vector3 boidLocation, Vector3 pathStart, Vector3 pathEnd)
+    {
+        Vector3 path = pathEnd - pathStart;
+        Vector3 startToBoid = boidLocation - pathStart;
+        path.Normalize();
+        path *= Vector3.Dot(startToBoid, path);
+        return pathStart + path;
+    }
 
     public void Flock()
     {
 
     }
 
+
+
+    //maps a value from one range to another
+    private float Map(float value, float originalMin, float originalMax, float newMin, float newMax)
+    {
+        return (newMin + (value - originalMin) * (newMax - newMin) / (originalMax - originalMin));
+    }
+
+    //return a predicted location of set distance ahead of player
+    public Vector3 PredictedLocation(float distance)
+    {
+        return transform.position+(rigidbody.velocity.normalized * distance);
+    }
+
+    float DistanceLineSegmentPoint(Vector3 a, Vector3 b, Vector3 p)
+    {
+        // If a == b line segment is a point and will cause a divide by zero in the line segment test.
+        // Instead return distance from a
+        if (a == b)
+            return Vector3.Distance(a, p);
+
+        // Line segment to point distance equation
+        Vector3 ba = b - a;
+        Vector3 pa = a - p;
+        return (pa - ba * (Vector3.Dot(pa, ba) / Vector3.Dot(ba, ba))).magnitude;
+    }
+
+    //https://forum.unity3d.com/threads/how-to-check-a-vector3-position-is-between-two-other-vector3-along-a-line.461474/
 }
